f2c11d727d0b6ce3c6ad2ac9898885145e2b2c25
LORENA1116 <RLORENA364@GMAIL.COM>
2021-05-20 20:12:09 -0500

    mi primer repositirio

---
+++ js/scrip.js
@@ -0,0 +1,50 @@
+
+$('#parrafo1').mouseover(function(){
+    $(this) .css("color", "pink")
+              .css("background", "black");
+})
+
+$('#parrafo1').mouseout(function(){
+    $(this).css("color", "black")
+        .css("background", "rgba(172, 170, 170, 0.534)");
+    
+})
+
+
+$('#parrafo2').mouseover(function(){
+    $(this) .css("color", "pink")
+              .css("background", "black");
+})
+
+$('#parrafo2').mouseout(function(){
+    $(this).css("color", "black")
+        .css("background", "rgba(172, 170, 170, 0.534)");
+    
+})
+
+
+$('#parrafo3').mouseover(function(){
+    $(this) .css("color", "pink")
+              .css("background", "black");
+})
+
+$('#parrafo3').mouseout(function(){
+    $(this).css("color", "black")
+        .css("background", "rgba(172, 170, 170, 0.534)");
+    
+})
+
+
+             
+
+$(".pais").mouseover(function(){
+$(this).css("color", "pink");
+$(this).css("font-size", "30px")
+})
+
+$(".pais").mouseout(function(){
+    $(this).css("color", "black");
+$(this).css("font-size", "20px")
+})
+
+$()
